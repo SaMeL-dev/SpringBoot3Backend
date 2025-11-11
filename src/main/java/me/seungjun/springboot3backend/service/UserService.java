ff24457f3c010f0242753d802d4c721beb63dcfd
516-08 <asraheesin@naver.com>
2025-11-11 16:30:39 +0900

    number 9

---
+++ src/main/java/me/seungjun/springboot3backend/service/UserService.java
@@ -0,0 +1,22 @@
+package me.seungjun.springboot3backend.service;
+
+import lombok.RequiredArgsConstructor;
+import me.seungjun.springboot3backend.domain.User;
+import me.seungjun.springboot3backend.dto.AddUserRequest;
+import me.seungjun.springboot3backend.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
