76583e2750aa91d53201daf2409c9a17849fb4c7
516-08 <asraheesin@naver.com>
2025-11-11 10:09:35 +0900

    initial commit

---
+++ src/main/java/me/seungjun/springboot3backend/SpringBoot3BackendApplication.java
@@ -0,0 +1,13 @@
+package me.seungjun.springboot3backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/seungjun/springboot3backend/SpringBoot3BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.seungjun.springboot3backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
